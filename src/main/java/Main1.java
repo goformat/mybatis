2f4cd9bfad1bcfcccacd8897ab2793c0666f9d14
wangshuai <1404482695@qq.com>
2019-11-10 10:38:49 +0800

    是打发的

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,9 +1,10 @@
-public class Main1 {
+﻿public class Main1 {
     public static void main(String[] args) {
         System.out.println("我是真的服了");
 		你在说什么
 		多唠叨,会影响你的幸福
 		不要太容易动怒
 		人生要活在当下
+这到底是什么鬼
     }
 }
